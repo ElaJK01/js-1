2c6ed18561e06c71a9945c3043bb3f972bdd2846
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-05-06 12:11:02 +0200

    first task

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,21 @@
 import { first } from "../data.js";
 
-const solve = () => {};
+const reduceElement = (firstElement) => {
+  const arrValue = firstElement.map((el) => Object.values(el)).map((i) => i[0]);
+  const reduceScore = arrValue.reduce((a, b) => a + b);
+  const namesList = firstElement
+    .map((el) => Object.values(el))
+    .map((i) => i[1])
+    .join(", ");
+  return {
+    totalScore: reduceScore,
+    names: "Team: " + namesList,
+  };
+};
+
+const solve = (...arr) => {
+  return arr.map((el) => reduceElement(el));
+};
+
 
 console.log(solve(...first));
